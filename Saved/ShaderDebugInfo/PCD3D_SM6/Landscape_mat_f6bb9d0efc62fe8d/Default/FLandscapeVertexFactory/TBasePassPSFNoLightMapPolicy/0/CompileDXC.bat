2e66e21cf636e0ec6de2c7e01e1454ba272e6128
Pimkamonw <kamonwan.srisawad@gmail.com>
2024-04-24 17:38:42 +1000

    Map 1 lanscape

--- Saved/ShaderDebugInfo/PCD3D_SM6/Landscape_mat_f6bb9d0efc62fe8d/Default/FLandscapeVertexFactory/TBasePassPSFNoLightMapPolicy/0/CompileDXC.bat
+++ Saved/ShaderDebugInfo/PCD3D_SM6/Landscape_mat_f6bb9d0efc62fe8d/Default/FLandscapeVertexFactory/TBasePassPSFNoLightMapPolicy/0/CompileDXC.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-SET DXC="C:\Program Files\Unreal\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+SET DXC="C:\Program Files\Epic Games\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
 IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "C:\Program Files\Unreal\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	ECHO Couldn't find dxc.exe under "C:\Program Files\Epic Games\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
 	GOTO :END
 )
 %DXC% /auto-binding-space 0 /Zpr /O3 -Wno-parentheses-equality -disable-lifetime-markers /T ps_6_6 /E MainPS /Fc BasePassPixelShader.d3dasm /Fo BasePassPixelShader.dxil BasePassPixelShader.usf
